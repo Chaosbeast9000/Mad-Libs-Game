e8784087dd6728e9b3a8a5d4e578c76405b9f11b
Chaosbeast9000 <64481348+Chaosbeast9000@users.noreply.github.com>
2021-08-19 09:18:49 -0400

    Add files via upload

---
+++ Mad Libs Game.py
@@ -0,0 +1,9 @@
+Mad Libs Game
+
+Yoda is a jedi (noun) who trained Jedi for (Noun: large number) years about how to use The (Noun). He is known for his (adjective) size, (adjective, color) skin and (adjective) size, (adjective, color) skin and (adjective) way of (verb -ing) words. In the movie Star Wars: A (adjective) Hope, Luke Skywalker (verb) s Yoda after (verb -ing) his (noun) into a (noun) on a (adjective) planet called (proper noun, place). Yoda (verb) s Luke to become a Jedi (noun). Yoda remains a (noun) to Luke, (verb -ing) him to use The Force to (verb) (Adjective) Vader and the Empire.
+First_noun = input ("Enter a noun: ")
+
+
+print("Yoda is a jedi", First_noun)
+
+Yoda is a jedi (noun) who trained Jedi for (Noun: large number) years about how to use The (Noun). He is known for his (adjective) size, (adjective, color) skin and (adjective) size, (adjective, color) skin and (adjective) way of (verb -ing) words. In the movie Star Wars: A (adjective) Hope, Luke Skywalker (verb) s Yoda after (verb -ing) his (noun) into a (noun) on a (adjective) planet called (proper noun, place). Yoda (verb) s Luke to become a Jedi (noun). Yoda remains a (noun) to Luke, (verb -ing) him to use The Force to (verb) (Adjective) Vader and the Empire.
